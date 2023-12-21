ec58fde2ca45e8df9662af860e8f47912ef6a33c
bone-house <bonehouse.dev@gmail.com>
2023-12-21 18:04:48 +0100

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
-import { useGamba } from 'gamba-react-v2'
+import { useGamba, type GameResult } from 'gamba-react-v2'
 import React from 'react'
+
+const tokens = {
+  sol: {
+    mint: "So11111111111111111111111111111111111111112",
+    decimals: 9,
+  }
+}
+
+const getToken = (mint: string) => {
+  const token = Object.entries(tokens).find(([k, v]) => mint === v.mint)
+  return token && token[1]
+}
 
 const tokenUnits = (amount: number, decimals = 9) => {
   return amount * (10 ** decimals)
@@ -11,28 +23,29 @@ const formatTokenAmount = (amount: number, decimals = 9) => {
 }
 
 export default function App() {
-  const [result, setResult] = React.useState<number | null>(null)
+  const [selectedToken] = React.useState(tokens.sol)
+  const [result, setResult] = React.useState<GameResult>()
   const gamba = useGamba()
 
   const play = async () => {
     // Start a bet
     await gamba.play({
-      // The potential outcomes we want for the bet (2x or 0x)
-      bet: [2, 0],
-      // Amount of coins
-      wager: tokenUnits(0.01),
+      // The potential outcomes for the bet
+      bet: [2, 0], // (2x or 0x)
+      // Amount of tokens
+      wager: tokenUnits(0.01, selectedToken.decimals),
       // The token we want to use for the bet (SOL is default)
-      token: "So11111111111111111111111111111111111111112",
-      // The address where fees will appear
+      token: selectedToken.mint,
+      // The address where fees will go to
       creator: "V2grJiwjs25iJYqumbHyKo5MTK7SFqZSdmoRaj8QWb9",
-      // Our platform will receive 1% of every bet
-      creatorFee: 0.01,
-      // This seed will affect the result, giving the player an option to change this value will assure them that the result is fair.
+      // The % amount that the creator address will receive for every bet
+      creatorFee: 0.01, // 0.01 = 1%
+      // This seed will affect the result. Giving the player an option to change this value will assure them that the result is fair.
       clientSeed: "anything goes",
     })
     // Get the result of the bet
     const result = await gamba.result()
-    setResult(result.payout)
+    setResult(result)
   }
 
   return (
@@ -42,10 +55,13 @@ export default function App() {
         Play
       </button>
       {gamba.isPlaying ? (
-        <>Rolling</>
-      ) : result !== null && (
+        "Rolling"
+      ) : result && (
         <>
-          {formatTokenAmount(result)}
+          {formatTokenAmount(
+            result.payout,
+            getToken(result.token.toBase58())?.decimals
+          )}
         </>
       )}
     </>
